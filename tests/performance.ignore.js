65f7d3a1f111660dd35b905e7d154078ef0df130
Dominic Cassidy <dcassidy@arago.de>
2020-01-20 16:41:45 +0000

    calculate performance linear regression

--- tests/performance.ignore.js
+++ tests/performance.ignore.js
@@ -4,6 +4,32 @@ const shell = require('shelljs')
 const fs = require('fs')
 const path = require('path')
 
+function calculateRegression(x, y, t) {
+  const sumX = x.reduce((acc, v) => acc + v, 0)
+  const sumY = y.reduce((acc, v) => acc + v, 0)
+
+  const avgX = sumX / x.length
+  const avgY = sumY / y.length
+
+  let sumSqX = 0
+  for (let i = 0; i < x.length; i++) {
+    sumSqX += Math.pow(x[i] - avgX, 2) // this will never change unless we add more tests...
+  }
+
+  let sumP = 0
+  for (let i = 0; i < y.length; i++) {
+    sumP += (x[i] - avgX) * (y[i] - avgY)
+  }
+
+  const b = sumP / sumSqX
+  const a = avgY - b * avgX
+  const result = `${b.toFixed(5)}X ${a > 0 ? '+' : '-'} ${a.toFixed(5)}`
+
+  // test something....
+
+  return result
+}
+
 test('Performance', function(t) {
   const res = shell.exec('wget -O - https://unpkg.com/nlp-corpus@3.3.0/builds/nlp-corpus-1.json')
   const outputPath = path.join(__dirname, './performance.results.json')
@@ -37,7 +63,9 @@ test('Performance', function(t) {
     console.groupEnd()
   }
 
-  const results = { x, y, key: { x: 'Length', y: 'Time' } }
+  const regression = calculateRegression(x, y, t)
+
+  const results = { x, y, key: { x: 'Length', y: 'Time' }, regression }
 
   fs.writeFileSync(outputPath, JSON.stringify(results, null, 2))
 
